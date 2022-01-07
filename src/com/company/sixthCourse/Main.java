4549f078006779e8f4a0f03e20c0815ebe5c776a
uitaibaby <uitaibaby88@mail.ru>
2022-01-07 13:19:36 +1000

    Last correct

--- src/com/company/sixthCourse/Main.java
+++ src/com/company/sixthCourse/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Dad Piter = new Dad("Piter", "Parker", "usa", 1965, true);
         Piter.passport();
-        Son Jack = new Son("jack", 2010, true);
+        Son Jack = new Son("Jack", 2010, true);
         Jack.passport();
         Daughter Mariya = new Daughter("Mariya", "Potter", 1993, false);
         Mariya.passport();
